ec771d5deed4c261bcb29bf9a8a45428de099ede
ivan <ivanfbermejo@gmail.com>
2025-08-29 20:21:16 +0200

    index.html

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public"))); 
 
 // --- Variables de entorno ---
 const {
