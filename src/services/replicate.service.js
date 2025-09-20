3f21dd2de6f290694f11a75545c3c7e4d5c0a7ed
ivan <ivanfbermejo@gmail.com>
2025-09-20 12:54:49 +0200

    change modelo replicate nano-banana

--- src/services/replicate.service.js
+++ src/services/replicate.service.js
@@ -5,8 +5,7 @@ const replicate = new Replicate({ auth: ENV.REPLICATE_API_TOKEN });
 
 export const ReplicateService = {
   async runStickerModel(imageUrl, prompt) {
-    // Sustituye "nano-banana:latest" por tu modelo real
-    return await replicate.run("nano-banana:latest", {
+    return await replicate.run("google/nano-banana", {
       input: { image: imageUrl, prompt }
     });
   }
